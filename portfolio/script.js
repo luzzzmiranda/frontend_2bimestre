0309a25b53a514358a586095d2f00aa290df6f7f
luzzmiranda <mirandamariana494@gmail.com>
2024-09-07 11:53:07 -0400

    ataulização

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+    } else {
+        menuMobile.classList.add('open')
+    }
+}
